b949fe3f60fa60c69ce7156608809774c47a2107
Sara Vahdatipour <54913233+lerunaway@users.noreply.github.com>
2023-03-02 17:26:09 +0100

    removed

--- Crawler/draft/main.py
+++
@@ -1,8 +0,0 @@
-from mypermissions import MyPermissions
-
-url = "https://www.permissionspolicy.xyz"
-
-my_permissions = MyPermissions(url)
-my_permissions.get_iframe_policy()
-my_permissions.get_features_to_check_conflict()
-my_permissions.check_permission_policy_conflicts()
